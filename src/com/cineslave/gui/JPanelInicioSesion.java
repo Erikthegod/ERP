1ed2dbf0d333cf0317d286319e7d4a5124f6cfd3
Erikthegod <Erikthegod@MacBook-Pro-de-Eric.local>
2016-12-02 16:44:58 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 465c34e26042e2f2a755cf66b23a5890dcf8b04b, reversing
    changes made to 4bc65561580bc64a2291ea07d23076f5edf80f5b.

--- src/com/cineslave/gui/JPanelInicioSesion.java
+++ src/com/cineslave/gui/JPanelInicioSesion.java
@@ -30,15 +30,10 @@ public class JPanelInicioSesion extends javax.swing.JPanel {
 
         jLabel1 = new javax.swing.JLabel();
         fefe = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jEditorPane1 = new javax.swing.JEditorPane();
 
         jLabel1.setText("Prueba Cambio");
 
         fefe.setText("cf");
-
-        jEditorPane1.setText("cosa incrediboy");
-        jScrollPane1.setViewportView(jEditorPane1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -46,20 +41,10 @@ public class JPanelInicioSesion extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(74, 74, 74)
-<<<<<<< HEAD
                 .addComponent(jLabel1)
                 .addGap(95, 95, 95)
                 .addComponent(fefe)
                 .addContainerGap(147, Short.MAX_VALUE))
-=======
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addGap(95, 95, 95)
-                        .addComponent(fefe)))
-                .addContainerGap(150, Short.MAX_VALUE))
->>>>>>> origin/master
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -71,17 +56,13 @@ public class JPanelInicioSesion extends javax.swing.JPanel {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(120, 120, 120)
                         .addComponent(fefe)))
-                .addGap(36, 36, 36)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(108, Short.MAX_VALUE))
+                .addContainerGap(166, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel fefe;
-    private javax.swing.JEditorPane jEditorPane1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
