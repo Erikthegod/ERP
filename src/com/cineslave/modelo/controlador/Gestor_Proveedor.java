68825be9f11d2a61a530277ee63e55e3d27cb353
adriaanpc23 <jguillen.adrianperomingo@gmail.com>
2017-01-18 17:24:59 +0100

    Algun cambio tonto

--- src/com/cineslave/modelo/controlador/Gestor_Proveedor.java
+++ src/com/cineslave/modelo/controlador/Gestor_Proveedor.java
@@ -70,21 +70,15 @@ public class Gestor_Proveedor {
     public Proveedor consultaProveedor(String _cif) throws SQLException {
         PreparedStatement ps;
         ResultSet rs = null;
-        Proveedor nuevoProveedor = null;
+        Proveedor _nuevoProveedor = null;
         String sql = "SELECT * FROM PROVEEDORES WHERE CIF = ?";
         ps = conexion.prepareStatement(sql);
         ps.setString(1, _cif);
         rs = ps.executeQuery();
         while (rs.next() == true) {
             //1 id//2 cif//3 nombre//4 telefono//5 poblacion// 6 cp
-            //System.out.println(rs.getInt(1));
-            System.out.println(rs.getString(2));
-            System.out.println(rs.getString(3));
-            System.out.println(rs.getInt(4));
-            System.out.println(rs.getString(5));
-            System.out.println(rs.getInt(6));
-            nuevoProveedor = new Proveedor(rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5), rs.getInt(6));
+            _nuevoProveedor = new Proveedor(rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5), rs.getInt(6));
         }
-        return nuevoProveedor;
+        return _nuevoProveedor;
     }
 }
